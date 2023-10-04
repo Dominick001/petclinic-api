d121571829c2c7b4e2dd479efabbc62c1e9287a3
Dominick001 <98152016+Dominick001@users.noreply.github.com>
2023-10-04 16:10:54 +0700

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 
 variable "vpc_id" {
   description =  "VPC to use for testing. Default is AWS Default VPC"
-  default = "vpc-442aaf21"
+  default = "vpc-02d8f7b6e2a856d9f"
 }
 
 
